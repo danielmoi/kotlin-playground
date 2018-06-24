07431265ccfc3e31ef0882260abf474acac75b08
danielmoi <daniel@ampersandmoi.com>
2018-06-24 13:40:49 +1000

    More interface fun

--- src/Vehicle/Vehicle.kt
+++ src/Vehicle/Vehicle.kt
@@ -1,32 +1,37 @@
 package Vehicle
 
 fun main(args: Array<String>) {
-   val car1 = Car(make = "audi", color = Blue, stuntMove = Burnout)
-    println("car1.wheels: ${car1.wheels}") 
-    println("car1.make: ${car1.make}")
-    println("car1.color.name: ${car1.color.name}")
+   val car1 = Car(make = "Ford", color = Blue, stuntMove = Burnout)
     car1.start()
     car1.accelerate()
     car1.stuntMove.go()
+    
+    println("----------")
+
+    val car2 = Car(make = "Holden", color = Red, stuntMove = Donut)
+    car2.start()
+    car2.accelerate()
+    car2.stuntMove.go()
 }
 
 abstract class Vehicle(val wheels: Int) {
     // All Vehicles get the start function
     // It cannot be overridden
     fun start() {
-        println("Starting...")
+        println("$make starting...")
     }
     // We don't give abstract class functions a body
     abstract fun accelerate()
 
     abstract val color: Color
+    abstract val make: String
 
     abstract val stuntMove: StuntMove
 
 
 }
 
-class Car(val make: String, override val color: Color, override val stuntMove: StuntMove): Vehicle(wheels = 2) {
+class Car(override val make: String, override val color: Color, override val stuntMove: StuntMove): Vehicle(wheels = 2) {
     // Implementation
     override fun accelerate() {
         println("Accelerating!!!!!!")
@@ -45,14 +50,19 @@ object Red: Color {
 
 
 interface StuntMove {
+    val name: String
     fun go()
 }
 object Burnout: StuntMove {
+    override val name: String
+        get() = "Burnout"
     override fun go() {
         println("Burnout!!!!!!!")
     }
 }
 object Donut: StuntMove {
+    override val name: String
+        get() = "Donut"
     override fun go() {
         println("DONUT!!!!!!!!!!!")
     }
